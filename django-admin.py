e2feb590568198bfd2df58f40e92830d9019c985
Sagari-VS <54168325+Sagari-VS@users.noreply.github.com>
2019-08-27 10:09:02 +0530

    Add files via upload
    user-registration

---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sagari/Desktop/userlogin/assignment1/assignment1/uservenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
